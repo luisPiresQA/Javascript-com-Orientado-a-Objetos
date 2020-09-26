e7354cfc95ee1448c992425e73b3154f7e5ae18f
luis.pires <luispiresqa@gmail.com>
2020-09-25 23:48:00 -0300

    Adicionando metodo sacar

--- index.js
+++ index.js
@@ -3,23 +3,41 @@ class Cliente{
 
     nome;
     cpf;
+    
+}
+
+class ContaCorrente{
+
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
  cliente1.nome = "Luis";
- cliente1.cpf = 11122233309;
- cliente1.agencia = 1001;
- cliente1.saldo = 0;
+ cliente1.cpf = 11122233309; 
 
-
+ const cliente2 = new Cliente();
  cliente2.nome = "Alice";
  cliente2.cpf = 888822233309;
- cliente2.agencia = 1001;
- cliente2.saldo = 0;
 
-console.log(cliente1);
-console.log(cliente2);
+ 
+const contaCorrenteLuis = new ContaCorrente();
+contaCorrenteLuis.saldo = 100;
+contaCorrenteLuis.agencia = 1001;
+
+console.log("Valor antes de colocar saldo:  "+contaCorrenteLuis.sacar(50));
+
+// contaCorrenteLuis.saldo = 100;
+
+
+
+console.log("Valor após colocar saldo: "+contaCorrenteLuis.saldo);
+// console.log(cliente1);
+// console.log(cliente2);
