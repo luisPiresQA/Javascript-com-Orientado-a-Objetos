b51f49acc0b9d3c29162ba11ca99c46926784ac3
luis.pires <luispiresqa@gmail.com>
2020-09-25 23:05:19 -0300

    Alterando a form de adicionar dados de cliente

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
+
+class Cliente{
+
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+ cliente1.nome = "Luis";
+ cliente1.cpf = 11122233309;
+ cliente1.agencia = 1001;
+ cliente1.saldo = 0;
 
 
-console.log(cliente1Nome);
+ cliente2.nome = "Alice";
+ cliente2.cpf = 888822233309;
+ cliente2.agencia = 1001;
+ cliente2.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
